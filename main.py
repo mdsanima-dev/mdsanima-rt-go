3534411ad456b5b3a31386c873e2c12f3c838846
mdsanima <marcinrozewski@gmail.com>
2021-08-13 18:16:20 +0200

    feat: simple first layout and `screen_switch` manager closes #6
    closes #7

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Main application MDSANIMA-RT-GO
 """
 
 
-from . import __version__
+from __init__ import __version__
 
 import kivy
 kivy.require('2.0.0')
@@ -30,24 +30,53 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 class MDSRTGO_scr_1(Screen):
     def __init__(self, **kwargs):
         super(MDSRTGO_scr_1, self).__init__(**kwargs)
-        pass
+        layout = FloatLayout()
+        btn = Button(text='SCREEN 2', on_release=self.screen_switch)
+
+        layout.add_widget(btn)
+        self.add_widget(layout)
+
+    def screen_switch(self, instance):
+        self.manager.current = 'scr_2'
+        self.manager.transition.direction = 'left'
 
 
 class MDSRTGO_scr_2(Screen):
     def __init__(self, **kwargs):
         super(MDSRTGO_scr_2, self).__init__(**kwargs)
-        pass
+        layout = FloatLayout()
+        btn = Button(text='SCREEN 3', on_release=self.screen_switch)
+
+        layout.add_widget(btn)
+        self.add_widget(layout)
+
+    def screen_switch(self, instance):
+        self.manager.current = 'scr_3'
+        self.manager.transition.direction = 'left'
 
 
 class MDSRTGO_scr_3(Screen):
     def __init__(self, **kwargs):
         super(MDSRTGO_scr_3, self).__init__(**kwargs)
-        pass
+        layout = FloatLayout()
+        btn = Button(text='SCREEN 1', on_release=self.screen_switch)
+
+        layout.add_widget(btn)
+        self.add_widget(layout)
+
+    def screen_switch(self, instance):
+        self.manager.current = 'scr_1'
+        self.manager.transition.direction = 'right'
 
 
 class MDSRTGO_main(MDApp):
+    title = "MDSANIMA RT GO v" + __version__
     def build(self):
-        pass
+        sm = ScreenManager()
+        sm.add_widget(MDSRTGO_scr_1(name='scr_1'))
+        sm.add_widget(MDSRTGO_scr_2(name='scr_2'))
+        sm.add_widget(MDSRTGO_scr_3(name='scr_3'))
+        return sm
 
 
 if __name__ == "__main__":
