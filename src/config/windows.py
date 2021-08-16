8059aa4fca474b6f52c9db929e94b62dc8e07b67
mdsanima <marcinrozewski@gmail.com>
2021-08-16 22:57:37 +0200

    fix: now app in windows and linux open on middle on the screen

--- src/config/windows.py
+++ src/config/windows.py
@@ -2,7 +2,7 @@ from kivy import Config
 
 
 # set configuration windows
-Config.set('graphics', 'width', '500')
+Config.set('graphics', 'width', '512')
 Config.set('graphics', 'height', '860')
 Config.set('graphics', 'minimum_width', '500')
 Config.set('graphics', 'minimum_height', '600')
