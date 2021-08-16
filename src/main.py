d56931497739553a4362e7af40b5bf6d87b796fb
mdsanima <marcinrozewski@gmail.com>
2021-08-16 22:42:46 +0200

    fix: add notification alert

--- src/main.py
+++ src/main.py
@@ -9,13 +9,13 @@ import kivy
 kivy.require('2.0.0')
 import config.windows
 
+from config.setting import check_platform
 from kivymd.app import MDApp
-from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen, ScreenManager
-from config.setting import check_platform
+from plyer import notification
 
 
 class MDSRTGO_scr_1(Screen):
@@ -64,6 +64,9 @@ class MDSRTGO_main(MDApp):
     title = "MDSANIMA RT GO v" + __version__
     def build(self):
         notification_icon = check_platform()
+        notification.notify(
+            'MDSANIMA RT GO', 'You have a 2 messages and 10 new issues',
+            app_icon=notification_icon)
         sm = ScreenManager()
         sm.add_widget(MDSRTGO_scr_1(name='scr_1'))
         sm.add_widget(MDSRTGO_scr_2(name='scr_2'))
