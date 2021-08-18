c34b0c58e626bd48285172ddb5d6c32a6660c53e
mdsanima <marcinrozewski@gmail.com>
2021-08-19 01:55:32 +0200

    fix(devs): clean up code enter after close the brackets in to all variable
    Variable `layout_box`

--- src/main.py
+++ src/main.py
@@ -59,30 +59,35 @@ class MDSRTGO_scr_1(Screen):
             'MDSANIMA RT GO '
             + '[b][color=#329EF4]'
             + 'v' + str(__version__)
-            + '[/color][/b]')
+            + '[/color][/b]'
+        )
 
-        bacground = Image(
+        background = Image(
             source=resource_path(img[1]), size=self.size, pos=self.pos,
             size_hint_y=None, size_hint_x=None,
             height='1849sp', width='1075sp',
             pos_hint={'center_x':0.5, 'center_y':0.5},
-            allow_stretch=True)
+            allow_stretch=True
+        )
 
         btn_go_rendering = MDFlatButton(
             text='GO RENDERING!', on_press=self.screen_switch,
             pos_hint={'center_x':0.5, 'center_y':0.6},
-            font_size='35')
+            font_size='35'
+        )
 
         btn_logo_mdsanima = MDIconButton(
             icon=resource_path(img[3]), on_press=self.screen_switch,
             user_font_size='80', size_hint_y=None,
-            pos_hint={'center_x':0.5, 'center_y':0.5})
+            pos_hint={'center_x':0.5, 'center_y':0.5}
+        )
 
         lbl_click_me = MDLabel(
             text='CLICK ME!', halign='center',
             pos_hint={'center_x':0.5, 'center_y':0.57},
             font_style='Caption',
-            theme_text_color='Secondary')
+            theme_text_color='Secondary'
+        )
 
         lbl_info_version = MDLabel(
             text=mdsanima_rt_go,
@@ -90,10 +95,11 @@ class MDSRTGO_scr_1(Screen):
             pos_hint={'center_x':0.5, 'center_y':0.02},
             font_style='Caption',
             theme_text_color='Secondary',
-            markup=True)
+            markup=True
+        )
 
         # add widget layout
-        layout.add_widget(bacground)
+        layout.add_widget(background)
         layout.add_widget(btn_go_rendering)
         layout.add_widget(btn_logo_mdsanima)
         layout.add_widget(lbl_click_me)
@@ -137,20 +143,23 @@ class MDSRTGO_scr_2(Screen):
             orientation='vertical',
             padding=0, spacing=0,
             size_hint_x=1, size_hint_y=0.3,
-            pos_hint={'center_x':0.5, 'top':0.8})
+            pos_hint={'center_x':0.5, 'top':0.8}
+        )
 
         mdsanima_rt_go = (
             'MDSANIMA RT GO '
             + '[b][color=#329EF4]'
             + 'v' + str(__version__)
-            + '[/color][/b]')
+            + '[/color][/b]'
+        )
 
-        bacground = Image(
+        background = Image(
             source=resource_path(img[2]), size=self.size, pos=self.pos,
             size_hint_y=None, size_hint_x=None,
             height='1849sp', width='1075sp',
             pos_hint={'center_x':0.5, 'center_y':0.5},
-            allow_stretch=True)
+            allow_stretch=True
+        )
 
         lbl_top_name = MDLabel(
             text='RENDER TIME CALCULATOR',
@@ -158,7 +167,8 @@ class MDSRTGO_scr_2(Screen):
             pos_hint={'center_x':0.5, 'center_y':0.96},
             font_style='H6',
             theme_text_color='Primary',
-            markup=True)
+            markup=True
+        )
 
         tfl_how_many_frame = MDTextField(
             required=True,
@@ -166,7 +176,8 @@ class MDSRTGO_scr_2(Screen):
             helper_text_mode='on_error',
             input_filter='int', max_text_length=6,
             pos_hint={'center_x':0.5, 'center_y':0.9},
-            size_hint_x=0.85)
+            size_hint_x=0.85
+        )
 
         lbl_render_frame = MDLabel(
             text='RENDER TIME ONE FRAME',
@@ -174,7 +185,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(170,40),
             pos_hint={'center_x':0.24, 'center_y':0.84},
             font_style='Body2',
-            theme_text_color='Hint')
+            theme_text_color='Hint'
+        )
 
         lbl_hours = MDLabel(
             text='HOURS:',
@@ -182,7 +194,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(70,40),
             pos_hint={'center_x':0.2, 'center_y':0.8},
             font_style='Button',
-            theme_text_color='Secondary')
+            theme_text_color='Secondary'
+        )
 
         lbl_minutes = MDLabel(
             text='MINUTES:',
@@ -190,7 +203,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(70,40),
             pos_hint={'center_x':0.2, 'center_y':0.7},
             font_style='Button',
-            theme_text_color='Secondary')
+            theme_text_color='Secondary'
+        )
 
         lbl_seconds = MDLabel(
             text='SECONDS:',
@@ -198,7 +212,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(70,40),
             pos_hint={'center_x':0.2, 'center_y':0.6},
             font_style='Button',
-            theme_text_color='Secondary')
+            theme_text_color='Secondary'
+        )
 
         lbl_hours_val = MDLabel(
             text='0',
@@ -206,7 +221,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(70,40),
             pos_hint={'center_x':0.23, 'center_y':0.8},
             font_style='Button',
-            theme_text_color='Error')
+            theme_text_color='Error'
+        )
 
         lbl_minutes_val = MDLabel(
             text='0',
@@ -214,7 +230,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(70,40),
             pos_hint={'center_x':0.23, 'center_y':0.7},
             font_style='Button',
-            theme_text_color='Error')
+            theme_text_color='Error'
+        )
 
         lbl_seconds_val = MDLabel(
             text='0',
@@ -222,25 +239,29 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(70,40),
             pos_hint={'center_x':0.23, 'center_y':0.6},
             font_style='Button',
-            theme_text_color='Error')
+            theme_text_color='Error'
+        )
 
         sld_hours_val = MDSlider(
             min=0, max=24, value=0,
             pos_hint={'center_x':0.5, 'center_y':0.78},
             size_hint_x=0.75,
-            color='#7c7c7c')
+            color='#7c7c7c'
+        )
 
         sld_minutes_val = MDSlider(
             min=0, max=60, value=0,
             pos_hint={'center_x':0.5, 'center_y':0.68},
             size_hint_x=0.75,
-            color='#7c7c7c')
+            color='#7c7c7c'
+        )
 
         sld_seconds_val = MDSlider(
             min=0, max=60, value=0,
             pos_hint={'center_x':0.5, 'center_y':0.58},
             size_hint_x=0.75,
-            color='#7c7c7c')
+            color='#7c7c7c'
+        )
 
         lbl_render_time = MDLabel(
             text='RENDER TIME',
@@ -248,7 +269,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(170,40),
             pos_hint={'center_x':0.24, 'center_y':0.48},
             font_style='Body2',
-            theme_text_color='Hint')
+            theme_text_color='Hint'
+        )
 
         lbl_render_start = MDLabel(
             text='START RENDER',
@@ -256,7 +278,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(170,40),
             pos_hint={'center_x':0.24, 'center_y':0.38},
             font_style='Body2',
-            theme_text_color='Hint')
+            theme_text_color='Hint'
+        )
 
         lbl_render_finish = MDLabel(
             text='FINISH RENDER',
@@ -264,7 +287,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(170,40),
             pos_hint={'center_x':0.24, 'center_y':0.28},
             font_style='Body2',
-            theme_text_color='Hint')
+            theme_text_color='Hint'
+        )
 
         lbl_render_time_val = MDLabel(
             text='10 minutes',
@@ -273,7 +297,8 @@ class MDSRTGO_scr_2(Screen):
             pos_hint={'center_x':0.515, 'center_y':0.44},
             font_style='H5',
             theme_text_color='Custom',
-            text_color = [.2510,.5529,.9765,1])
+            text_color = [.2510,.5529,.9765,1]
+        )
 
         lbl_render_start_val = MDLabel(
             text='2021-08-18  23:43:44  Wednesday',
@@ -281,7 +306,8 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(450,40),
             pos_hint={'center_x':0.515, 'center_y':0.34},
             font_style='H6',
-            theme_text_color='Secondary')
+            theme_text_color='Secondary'
+        )
 
         lbl_render_finish_val = MDLabel(
             text='2021-08-18  23:53:44  Wednesday',
@@ -289,12 +315,14 @@ class MDSRTGO_scr_2(Screen):
             size_hint=(None,None), size=(450,40),
             pos_hint={'center_x':0.515, 'center_y':0.24},
             font_style='H6',
-            theme_text_color='Secondary')
+            theme_text_color='Secondary'
+        )
 
         btn_app_info = MDFlatButton(
             text='app info', size_hint=(None,None), size=(101,40),
             pos_hint={'x':0.416,'y':0.03},
-            on_release=self.screen_switch)
+            on_release=self.screen_switch
+        )
 
         lbl_info_version = MDLabel(
             text=mdsanima_rt_go,
@@ -302,10 +330,11 @@ class MDSRTGO_scr_2(Screen):
             pos_hint={'center_x':0.5, 'center_y':0.02},
             font_style='Caption',
             theme_text_color='Secondary',
-            markup=True)
+            markup=True
+        )
 
         # add widget layout
-        layout.add_widget(bacground)
+        layout.add_widget(background)
 
         layout.add_widget(lbl_top_name)
         layout.add_widget(tfl_how_many_frame)
@@ -357,18 +386,25 @@ class MDSRTGO_scr_3(Screen):
         layout = MDFloatLayout()
         img = get_images()
 
+        layout_box = MDBoxLayout(
+            padding='60sp',
+            orientation='vertical'
+        )
+
         mdsanima_rt_go = (
             'MDSANIMA RT GO '
             + '[b][color=#329EF4]'
             + 'v' + str(__version__)
-            + '[/color][/b]')
+            + '[/color][/b]'
+        )
 
-        bacground = Image(
+        background = Image(
             source=resource_path(img[2]), size=self.size, pos=self.pos,
             size_hint_y=None, size_hint_x=None,
             height='1849sp', width='1075sp',
             pos_hint={'center_x':0.5, 'center_y':0.5},
-            allow_stretch=True)
+            allow_stretch=True
+        )
 
         btn_back_to_main = MDIconButton(
             on_press=self.screen_switch,
@@ -377,7 +413,8 @@ class MDSRTGO_scr_3(Screen):
             icon='arrow-left',
             theme_text_color='Custom',
             text_color=[.2510,.5529,.9765,1],
-            md_bg_color=[0,0,0,0])
+            md_bg_color=[0,0,0,0]
+        )
 
         lbl_info_version = MDLabel(
             text=mdsanima_rt_go,
@@ -385,10 +422,11 @@ class MDSRTGO_scr_3(Screen):
             pos_hint={'center_x':0.5, 'center_y':0.02},
             font_style='Caption',
             theme_text_color='Secondary',
-            markup=True)
+            markup=True
+        )
 
         # add widget layout
-        layout.add_widget(bacground)
+        layout.add_widget(background)
         layout.add_widget(btn_back_to_main)
         layout.add_widget(lbl_info_version)
 
@@ -404,19 +442,24 @@ class MDSRTGO_main(MDApp):
     title = 'MDSANIMA RT GO v' + __version__
     def build(self):
         theme_kivy(self, 'Orange', 'Blue', 'Dark')
+
         img = get_images()
         self.icon = resource_path(img[0])
+
         notification_icon = check_platform()
         notification.notify(
             title='MDSANIMA RT GO',
             message='You have a 2 messages and 10 new issues',
             app_name='MDSANIMA RT GO',
             app_icon=resource_path(notification_icon),
-            timeout=10)
+            timeout=10
+        )
+
         sm = ScreenManager()
         sm.add_widget(MDSRTGO_scr_1(name='scr_1'))
         sm.add_widget(MDSRTGO_scr_2(name='scr_2'))
         sm.add_widget(MDSRTGO_scr_3(name='scr_3'))
+
         return sm
 
 
