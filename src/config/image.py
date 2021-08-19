ef4f669750c702cb799bc35511f8e2956e42c5d8
mdsanima <marcinrozewski@gmail.com>
2021-08-20 00:11:48 +0200

    fix: background folder name

--- src/config/image.py
+++ src/config/image.py
@@ -4,8 +4,8 @@ Set images variables
 
 
 app_icon = 'image/ic_launcher/res/mipmap-xhdpi/ic_launcher.png'
-bg_welcome = 'image/bacground/bg_1.png'
-bg_main = 'image/bacground/bg_2.png'
+bg_welcome = 'image/background/bg_1.png'
+bg_main = 'image/background/bg_2.png'
 logo_web = 'image/ic_launcher/web_hi_res_512.png'
 ascii_mdsanima = 'image/ic_launcher/ascii_mdsanima.png'
 
